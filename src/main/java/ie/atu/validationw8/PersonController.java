81608696ea8d8a1066128d83ffa376fa5e193dbe
G00401655@atu.ie <G00401655@atu.ie>
2023-11-15 15:38:16 +0000

    created createPerson, employeeId

--- src/main/java/ie/atu/validationw8/PersonController.java
+++ src/main/java/ie/atu/validationw8/PersonController.java
@@ -1,8 +1,11 @@
 package ie.atu.validationw8;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/person")
 @RestController
@@ -12,6 +15,32 @@ public class PersonController {
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
+
+    @PostMapping("/createPerson")
+    public  ResponseEntity<String>create(@Valid @RequestBody Person person){
+        personService.savePerson(person);
+        return new ResponseEntity<>("Person created", HttpStatus.OK);
+
+
+    }
+
+
+
+    @GetMapping("/{employeeId}")
+    public ResponseEntity<?> getPerson(@PathVariable String employeeId) {
+        if (employeeId.length() > 5 || employeeId.isBlank()){
+            return ResponseEntity.badRequest().body("EmployeeId is Invalid");
+
+        }
+        Person person = personService.getPersonByEmplyeeId(employeeId);
+
+        if(person == null) {
+            return ResponseEntity.notFound().build();
+
+        }
+        return ResponseEntity.ok(person);
+    }
+
 
 
 }
